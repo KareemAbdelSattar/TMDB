12b0ee8d40db7bc7e7545007101e47a7cb4e4d02
KareemAbdElSattar <kareem.as.aboelfateh@gmail.com>
2024-01-28 13:06:05 +0200

    :bug: Fix issue in Movie Details Path

--- TMDB/Modules/MovieDetails/Data/DataSource/MovieDetailsEndPoint.swift
+++ TMDB/Modules/MovieDetails/Data/DataSource/MovieDetailsEndPoint.swift
@@ -3,7 +3,7 @@ import Foundation
 struct MovieDetailsEndPoint {
     static func getMovieDetails(id: Int) -> EndPoint<MovieDetailsDTO> {
         EndPoint(
-            path: "discover/movie/\(id)",
+            path: "movie/\(id)",
             method: .GET
         )
     }
